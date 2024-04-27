af14965b1996b6da0a48f872508c6c7a7685f566
InvicTheBrazilian <130802656+InvicTheBrazilian@users.noreply.github.com>
2024-04-26 21:25:47 -0300

    Update Ex04_Main.kt

--- prova01/Ex04_Main.kt
+++ prova01/Ex04_Main.kt
@@ -1,3 +1,27 @@
+//Crie uma classe Carta que represente uma carta de baralho com atributos como naipe e valor.
+//Crie uma classe Baralhoque represente um baralho de cartas completo.
+//Implemente métodos para embaralhar o baralho, distribuir cartas.
+//Entregue 5 cartas para um Jogador que vai verificar se as cartas possuem combinações vencedoras,
+//como:
+
+//Par: Duas cartas com mesmo valor
+
+//Exemplo: A♠️ + A♥️
+
+//Trinca: Três cartas com mesmo valor
+
+//Exemplo: A♠️ + A♥️ + A♦️
+
+//Full House: Três cartas com mesmo valor mais outras duas com mesmo valor
+
+//Exemplo: A♠️ + A♥️ + A♦️+ 5♣️ + 5♥️
+
+//Flush: Todas cartas com mesmo naipe
+
+//Exemplo: A♦️+ 3♦️+ 6♦️ + Q♦️ + 10♦️
+
+//Equipe: Caio Tobias Garcia
+
 fun main() {
     val baralho = Baralho()
     baralho.embaralhar()
